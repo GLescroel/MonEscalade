664b5cfeaa239212e4b88a2da1330ef9249ff51a
GREGORY LESCROEL <gregory.lescroel@axa.fr>
2019-10-09 10:12:15 +0200

    Initialisation Spring Security

---
+++ src/main/java/glescroel/escalade/config/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package glescroel.escalade.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/**.jpg", "/**.png", "/**.css").permitAll()
+                .antMatchers("/monprofil").hasAuthority("USER")
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successForwardUrl("/")
+                .failureForwardUrl("/login-error")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+                .and().rememberMe();
+                //.and().csrf().disable();
+    }
+
+    /**
+     * Déclaration du fournisseur d'authentification
+     *
+     * @return l'instance du fournisseur d'authentification
+     */
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        final DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder());
+        return authProvider;
+    }
+
+    /**
+     * Déclaration du bean utilisé par le fournisseur d'authentification pour encoder les mdp.
+     *
+     * @return le bean chargé d'encodé les mdp
+     */
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+
+}
