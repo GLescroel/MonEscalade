b076b826d14b0c9d9d17126a108ef14593c2db9f
GREGORY LESCROEL <gregory.lescroel@axa.fr>
2019-10-11 11:17:08 +0200

    Démarrage de la HomePage : recherche d'un site par nom partiel / continent / pays

--- src/main/java/glescroel/escalade/repository/SiteRepository.java
+++ src/main/java/glescroel/escalade/repository/SiteRepository.java
@@ -2,8 +2,11 @@ package glescroel.escalade.repository;
 
 import glescroel.escalade.model.Site;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +14,34 @@ public interface SiteRepository extends JpaRepository<Site, Integer> {
 
     Optional<Site> findByNomIgnoreCase(String nom);
 
+    @Query("select site from Site site " +
+            "where site.nom LIKE %:nomPartiel% " +
+            "order by site.nom asc")
+    List<Site> getSiteByNomPartiel(@Param("nomPartiel") String nomPartiel);
+
+    @Query("select site from Site site " +
+            "where site.localisation in (select localisation from Localisation localisation " +
+                                            "where localisation.continent.id = :continentRecherche) " +
+            "order by site.nom asc")
+    List<Site> getSiteByContinent(@Param("continentRecherche") Integer continentRecherche);
+
+    @Query("select site from Site site " +
+            "where site.localisation in (select localisation from Localisation localisation " +
+            "where localisation.pays.id = :paysRecherche) " +
+            "order by site.nom asc")
+    List<Site> getSiteByPays(@Param("paysRecherche") Integer paysRecherche);
+
+    @Query("select site from Site site " +
+            "where site.nom LIKE %:nomPartiel% and " +
+            "site.localisation in (select localisation from Localisation localisation " +
+            "where localisation.continent.id = :continentRecherche) " +
+            "order by site.nom asc")
+    List<Site> getSitesByNomPartielAndContinent(@Param("nomPartiel") String nomPartiel, @Param("continentRecherche") Integer continentRecherche);
+
+    @Query("select site from Site site " +
+            "where site.nom LIKE %:nomPartiel% and " +
+            "site.localisation in (select localisation from Localisation localisation " +
+            "where localisation.continent.id = :paysRecherche) " +
+            "order by site.nom asc")
+    List<Site> getSitesByNomPartielAndPays(@Param("nomPartiel") String nomPartiel, @Param("paysRecherche") Integer paysRecherche);
 }
--- src/main/java/glescroel/escalade/service/SiteService.java
+++ src/main/java/glescroel/escalade/service/SiteService.java
@@ -6,6 +6,8 @@ import glescroel.escalade.repository.SiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,14 +16,45 @@ public class SiteService {
     @Autowired
     private SiteRepository siteRepository;
 
-
-
-    public SiteDto getSiteByNom(String nom){
+    public SiteDto getSiteByNom(String nom) {
         Optional<Site> result = siteRepository.findByNomIgnoreCase(nom);
         SiteDto siteDto = null;
-        if (result.isPresent()){
+        if (result.isPresent()) {
             siteDto = new SiteDto(result.get());
         }
         return siteDto;
+    }
+
+    public List<SiteDto> getSitesByNomPartiel(String nomPartiel) {
+        List<Site> sites = siteRepository.getSiteByNomPartiel(nomPartiel);
+        return convertSitesToDtos(sites);
+    }
+
+    public List<SiteDto> getSitesByContinent(Integer continentRecherche) {
+        List<Site> sites = siteRepository.getSiteByContinent(continentRecherche);
+        return convertSitesToDtos(sites);
+    }
+
+    public List<SiteDto> getSitesByPays(Integer paysRecherche) {
+        List<Site> sites = siteRepository.getSiteByPays(paysRecherche);
+        return convertSitesToDtos(sites);
+    }
+
+    public List<SiteDto> getSitesByNomPartielAndContinent(String nomPartiel, Integer continentRecherche) {
+        List<Site> sites = siteRepository.getSitesByNomPartielAndContinent(nomPartiel, continentRecherche);
+        return convertSitesToDtos(sites);
+    }
+
+    public List<SiteDto> getSitesByNomPartielAndPays(String nomPartiel, Integer paysRecherche) {
+        List<Site> sites = siteRepository.getSitesByNomPartielAndPays(nomPartiel, paysRecherche);
+        return convertSitesToDtos(sites);
+    }
+
+    private List<SiteDto> convertSitesToDtos(List<Site> sites) {
+        List<SiteDto> siteDtos = new ArrayList();
+        for (Site site : sites){
+            siteDtos.add(new SiteDto(site));
+        }
+        return siteDtos;
     }
 }
